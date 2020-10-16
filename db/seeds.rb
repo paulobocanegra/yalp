8c65f910e43890476b63a24abfcac7237cd8c588
paulobocanegra <paulobocanegra@gmail.com>
2020-10-16 11:48:53 -0700

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create(email:"omar@omar.com", first_name:"Omar", last_name:"LastName", zip_code:"94523")
-User.create(email:"diego@diego.com", first_name:"Diego", last_name:"LastName", zip_code:"94523")
+User.create(email:"omar@yalp.com", first_name:"Omar", last_name:"Fernandez", zip_code:"94510")
+User.create(email:"diego@yalp.com", first_name:"Diego", last_name:"Perez", zip_code:"94510")
+User.create(email:"karen@yalp.com", first_name:"Karen", last_name:"Smith", zip_code:"94523")
+User.create(email:"nancy@yalp.com", first_name:"Nancy", last_name:"Morrow", zip_code:"94523")
 User.create(email:"testuser@yalp.com", first_name:"testUser", last_name:"LastName", zip_code:"94523")
