f738bc72a201a142f445e0b321838b87cd4716ba
paulobocanegra <paulobocanegra@gmail.com>
2020-10-20 22:18:52 -0700

    AWS initiated

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,7 @@
 class Business < ApplicationRecord
     validates :name, :bio, :phone_number, :location, :hours, presence:true
     validates :rating, inclusion:{in: (1..5)}
+
+    has_many_attached :photos
 end
 
