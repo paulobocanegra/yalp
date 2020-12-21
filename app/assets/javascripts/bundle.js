bb47c8e57b06397256c84199d15be074672c60be
paulobocanegra <paulobocanegra@gmail.com>
2020-12-21 10:38:10 -0800

    User profile styling

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -2865,6 +2865,8 @@ var Profile = /*#__PURE__*/function (_React$Component) {
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_header_container__WEBPACK_IMPORTED_MODULE_2__["default"], null)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "profile-container"
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "profile-content-container"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "left-most-container"
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "profile-picture-user-show"
@@ -2893,7 +2895,7 @@ var Profile = /*#__PURE__*/function (_React$Component) {
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
         className: "update-link",
         id: "bottom-link"
-      }, "Change Password"))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, "Change Password")))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "profile-content"
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "under-profile-picture"
--- frontend/components/user/profile.jsx
+++ frontend/components/user/profile.jsx
@@ -37,45 +37,47 @@ class Profile extends React.Component{
                     <HeaderContainer/>
                 </div>
                 <div className="profile-container">
-                    <div className="left-most-container">
-                        <div className="profile-picture-user-show">
-                        </div>
-                    </div>
-                    <div className="profile-user-info">
-                        <div>
-                            <h1 className="user-username">{this.props.currentUser.first_name}</h1>
-                            <h3>{this.props.currentUser.zip_code}</h3>
-                            <br/>
-                            <div className="reviews-holder">  
-                                <i className="fas fa-star" />
-                                <div className="number-reviews">{Object.values(this.props.reviews).length} Reviews</div>
+                    <div className="profile-content-container">
+                        <div className="left-most-container">
+                            <div className="profile-picture-user-show">
                             </div>
-
                         </div>
-                    </div>
-                    <div className="update-nav-bar user-actions">
-                        {/* <div>
-                            <p className="account-settings">{this.state.first_name}Settings </p>
-                        </div> */}
-                        <ul className="profile-list-holder">
-                            <Link className="header-link-profile" to={`/users/${this.props.currentUser.id}/update`}>
-                                <li className="update-link" >Update Profile</li>
-                            </Link>
-                            <Link className="header-link-profile" to={`/users/${this.props.currentUser.id}/update-password`}>
-                                <li className="update-link" id="bottom-link">Change Password</li>
-                            </Link>
+                        <div className="profile-user-info">
+                            <div>
+                                <h1 className="user-username">{this.props.currentUser.first_name}</h1>
+                                <h3>{this.props.currentUser.zip_code}</h3>
+                                <br/>
+                                <div className="reviews-holder">  
+                                    <i className="fas fa-star" />
+                                    <div className="number-reviews">{Object.values(this.props.reviews).length} Reviews</div>
+                                </div>
 
-                            {/* <li className="update-link" id="selected-link">
-                                    <Link className="header-link-profile" to="">Profile</Link>
-                                </li>
-                                <li className="update-link" id="bottom-link">
-                                    <Link className="header-link-profile" to="">Password</Link>
-                                </li> */}
-                        </ul>
+                            </div>
+                        </div>
+                        <div className="update-nav-bar user-actions">
+                            {/* <div>
+                                <p className="account-settings">{this.state.first_name}Settings </p>
+                            </div> */}
+                            <ul className="profile-list-holder">
+                                <Link className="header-link-profile" to={`/users/${this.props.currentUser.id}/update`}>
+                                    <li className="update-link" >Update Profile</li>
+                                </Link>
+                                <Link className="header-link-profile" to={`/users/${this.props.currentUser.id}/update-password`}>
+                                    <li className="update-link" id="bottom-link">Change Password</li>
+                                </Link>
+
+                                {/* <li className="update-link" id="selected-link">
+                                        <Link className="header-link-profile" to="">Profile</Link>
+                                    </li>
+                                    <li className="update-link" id="bottom-link">
+                                        <Link className="header-link-profile" to="">Password</Link>
+                                    </li> */}
+                            </ul>
+                        </div>
+                        {/* <div className="user-actions">
+                            <Link to={`/users/${this.props.currentUser.id}/update`}>Update Profile</Link>
+                        </div> */}
                     </div>
-                    {/* <div className="user-actions">
-                        <Link to={`/users/${this.props.currentUser.id}/update`}>Update Profile</Link>
-                    </div> */}
                 </div>
                 <div className="profile-content">
                     <div className="under-profile-picture">
